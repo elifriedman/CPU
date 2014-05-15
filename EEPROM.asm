8a0c49ba1b75ce4c71631af058eda8f7aa4e4119
Krittian <friedm3@cooper.edu>
2014-05-15 03:06:50 -0400

    changed stuff

--- EEPROM.asm
+++ EEPROM.asm
@@ -19,6 +19,7 @@
 
 
 
+
 readReg ; Read register, assuming non-offset reg address is in W already
     addlw   R0  ; offset to actual register locations
     movwf   FSR ; indirect addressing
@@ -34,9 +35,9 @@ writeReg ; Write to register, assuming non-offset reg address is in W already
 
 writeStatus ; sets R15, the Status register [- - Z C]
     clrf    R15
-    btfss   PORTA,ZZ ;get status from PORTA
+    btfsc   PORTA,CC ;get status from PORTA
         bsf   R15,0  ; set C bit
-    btfss   PORTA,CC
+    btfsc   PORTA,ZZ
         bsf   R15,1  ; set Z bit
     return
 
@@ -66,37 +67,38 @@ readInstr ; Read Instruction from EEPROM
 
 execInstr ; Execute Instruction
     movf    INSTR1,W
-    andlw   0xF0    ; extract upper 4 bits
+    andlw   0xF0    ; extract upper 4 bits = kirt_commands
+    btfss INSTR1, 7; test MSb if set skip zcommands
+    goto zcomands
 
-    xorlw   NOP2    ; xoring will make W=0 and Z=1 if they match
-    btfsc   STATUS,Z
-    goto    inop2
-    xorlw   ADD^NOP2 ; :::::: ADD
-    btfsc   STATUS,Z
-    goto    iadd
+    ;;now test the one_commands
 
-    xorlw   SUB^ADD  ; :::::: SUB
-    btfsc   STATUS,Z
-    goto    isub
+zcomands ;testing if it's the nop command or if it's rot or ALU
+    ;;the kirtcommands are in w
 
-    xorlw   AND^SUB  ; :::::: AND
-    btfsc   STATUS,Z
-    goto    iand
-
-    xorlw   IOR^AND  ; :::::: OR
-    btfsc   STATUS,Z
-    goto    ior
-
-    xorlw   XOR^IOR  ; :::::: XOR
-    btfsc   STATUS,Z
-    goto    ixor
-
-inop2 ; NOP instruction
+    xorlw NOP2 ; xoring will make W=0 and Z=1 if they match
+    btfsc STATUS,Z
     goto start
 
-iadd ; ADD instruction
+    xorlw RRL^NOP2 ; :::::: RRL or RRR
+    btfsc STATUS,Z
+    goto iRotate
+
+    ;do ALU COMMANDS
+    xorlw RRL; get the commands back to right ALU commands
+    goto    iALU
+
+iRotate
+    ;;do rotating commands in ALU;
+
+    goto start;
+
+iALU ; ALU instruction
+    banksel PORTC
+    movwf   PORTC; put the comand on PORTC for the ALU
 
 ; Write Rc to PORTB<7:4>
+    banksel INSTR2
     movf    INSTR2,W
     andlw   0x0F ;
     call    readReg ; get Rc
@@ -114,9 +116,8 @@ iadd ; ADD instruction
     iorwf   PORTB,F ; PORTB = ccccbbbb
 
 ; Get result from ALU
-    movlw   ADD
-    movwf   PORTC ; ALU Add
     movf    PORTA,W
+
 ; Write Z,C
     call    writeStatus
 
@@ -131,155 +132,9 @@ iadd ; ADD instruction
     
     goto start
 
-isub ; SUB instruction
- ; Write Rc to PORTB<7:4>
-    movf    INSTR2,W
-    andlw   0x0F ;
-    call    readReg ; get Rc
-    banksel PORTB
-    movwf   PORTB
-    swapf   PORTB,F ; PORTB = cccc0000
-
-; Write Rb to PORTB<3:0>
-    banksel INSTR2
-    swapf   INSTR2,F
-    movf    INSTR2,W
-    andlw   0x0F
-    call    readReg ; get Rb
-    banksel PORTB
-    iorwf   PORTB,F ; PORTB = ccccbbbb
-
-; Get result from ALU
-    movlw   SUB
-    movwf   PORTC ; ALU Add
-    movf    PORTA,W
-; Write Z,C
-    call    writeStatus
-
-; Write to Ra
-    banksel INSTR1
-    andlw   0x0F
-    movwf   writeVal
-    movf    INSTR1,W
-    andlw   0x0F ;
-
-    call    writeReg ; Ra = Rb + Rc
-
-    goto start
-
-iand ; SUB instruction
- ; Write Rc to PORTB<7:4>
-    movf    INSTR2,W
-    andlw   0x0F ;
-    call    readReg ; get Rc
-    banksel PORTB
-    movwf   PORTB
-    swapf   PORTB,F ; PORTB = cccc0000
-
-; Write Rb to PORTB<3:0>
-    banksel INSTR2
-    swapf   INSTR2,F
-    movf    INSTR2,W
-    andlw   0x0F
-    call    readReg ; get Rb
-    banksel PORTB
-    iorwf   PORTB,F ; PORTB = ccccbbbb
-
-; Get result from ALU
-    movlw   AND
-    movwf   PORTC ; ALU Add
-    movf    PORTA,W
-; Write Z,C
-    call    writeStatus
-
-; Write to Ra
-    banksel INSTR1
-    andlw   0x0F
-    movwf   writeVal
-    movf    INSTR1,W
-    andlw   0x0F ;
-
-    call    writeReg ; Ra = Rb + Rc
-
-    goto start
-
-ior ; SUB instruction
- ; Write Rc to PORTB<7:4>
-    movf    INSTR2,W
-    andlw   0x0F ;
-    call    readReg ; get Rc
-    banksel PORTB
-    movwf   PORTB
-    swapf   PORTB,F ; PORTB = cccc0000
-
-; Write Rb to PORTB<3:0>
-    banksel INSTR2
-    swapf   INSTR2,F
-    movf    INSTR2,W
-    andlw   0x0F
-    call    readReg ; get Rb
-    banksel PORTB
-    iorwf   PORTB,F ; PORTB = ccccbbbb
-
-; Get result from ALU
-    movlw   IOR
-    movwf   PORTC ; ALU Add
-    movf    PORTA,W
-; Write Z,C
-    call    writeStatus
-
-; Write to Ra
-    banksel INSTR1
-    andlw   0x0F
-    movwf   writeVal
-    movf    INSTR1,W
-    andlw   0x0F ;
-
-    call    writeReg ; Ra = Rb + Rc
-
-    goto start
-
-ixor ; SUB instruction
- ; Write Rc to PORTB<7:4>
-    movf    INSTR2,W
-    andlw   0x0F ;
-    call    readReg ; get Rc
-    banksel PORTB
-    movwf   PORTB
-    swapf   PORTB,F ; PORTB = cccc0000
-
-; Write Rb to PORTB<3:0>
-    banksel INSTR2
-    swapf   INSTR2,F
-    movf    INSTR2,W
-    andlw   0x0F
-    call    readReg ; get Rb
-    banksel PORTB
-    iorwf   PORTB,F ; PORTB = ccccbbbb
-
-; Get result from ALU
-    movlw   XOR
-    movwf   PORTC ; ALU Add
-    movf    PORTA,W
-; Write Z,C
-    call    writeStatus
-
-; Write to Ra
-    banksel INSTR1
-    andlw   0x0F
-    movwf   writeVal
-    movf    INSTR1,W
-    andlw   0x0F ;
-
-    call    writeReg ; Ra = Rb + Rc
-
-    goto start
-
-
-
 
 start
-    call    readInstr;
-    goto    execInstr;
+    call    readInstr
+    goto    execInstr
     goto    start
     end
