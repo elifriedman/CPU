48ba000543b3abbb6935eafb328ff068bebc4a41
Krittian <friedm3@cooper.edu>
2014-05-15 00:55:39 -0400

    added netbeans project stuff

---
+++ build/default/debug/EEPROM.o.d
@@ -0,0 +1,4 @@
+build/default/debug/EEPROM.o:  \
+	/Users/EliFriedman/Documents/School/Sophomore/CompArch/Computer/EEPROM.asm  \
+	/Users/EliFriedman/Documents/School/Sophomore/CompArch/Computer/constants.inc  \
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Computer.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Computer.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=computer.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/computer.tar
